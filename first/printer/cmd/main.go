63698a18c80971cc4bef9c1675db74371333de71
jelabdel <jelabdel@gmail.com>
2020-07-27 19:57:59 +0200

    first library creation

---
+++ first/printer/cmd/main.go
@@ -0,0 +1,8 @@
+package main
+
+// Automatically imports!! vs code uses goimports tool underneath to do this
+import "github.com/jelabdel/learngo/first/printer"
+
+func main() {
+	printer.Hello()
+}
